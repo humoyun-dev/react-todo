4863551dc5495d9ae1a3ded4bf7dc086e79a0a3b
humoyun-dev <humoyunbektursunniyazov@gmail.com>
2024-09-11 16:30:03 +0500

    finish functional side

--- src/app.js
+++ src/app.js
@@ -4,13 +4,46 @@ import TodoList from "./list";
 
 export default function App() {
   const [todo, setToDo] = useState([]);
+  const [bin, setBin] = useState([]);
+
+  function ondDelete(index) {
+    const data = bin.filter((_, i) => i !== index);
+
+    setBin(data)
+  }
+
+  function onRestore (item, index) {
+    const binData = bin.filter((_, i) => i !== index);
+
+    const data = [...todo, item]
+
+    setBin(binData)
+    setToDo(data);
+  }
 
   return (
     <main>
       <div className="todo-body">
         <ToDoForm todo={todo} setToDo={setToDo} />
-        <input type="search" />
-        <TodoList todo={todo} setToDo={setToDo} />
+        <TodoList bin={bin} setBin={setBin} todo={todo} setToDo={setToDo} />
+        <hr />
+        <ul className="todo-list">
+          {bin.map((item, index) => (
+            <li key={index}>
+              <p>
+                {index + 1}: {item.done ? <del>{item.title}</del> : item.title}
+              </p>
+              <div className="list-options">
+                <button onClick={()=>onRestore(item, index)}>
+                  <i className="bi bi-upload"></i>
+                </button>
+                <button onClick={()=>ondDelete(index)}>
+                  <i className="bi bi-trash3"></i>
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
--- src/list.js
+++ src/list.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
 
-const TodoList = ({ todo, setToDo }) => {
+const TodoList = ({ todo, setToDo, bin, setBin }) => {
   const [edit, setEdit] = useState(null);
   const [editTitle, setEditTitle] = useState("");
+  const [search, setSearch] = useState("");
+
+  const searchedData = todo.filter((item) =>
+    item.title.replaceAll(" ", "").toLowerCase().includes(search.replaceAll(" ", "").toLowerCase())
+  );
 
   function onDoneTask(index, done) {
     const data = todo.map((item, i) => {
@@ -17,8 +22,12 @@ const TodoList = ({ todo, setToDo }) => {
     setToDo(data);
   }
 
-  function ondDelete(index) {
+  function ondDelete(item, index) {
     const data = todo.filter((_, i) => i !== index);
+
+    const binData = [...bin, item]
+
+    setBin(binData)
     setToDo(data);
   }
 
@@ -26,50 +35,61 @@ const TodoList = ({ todo, setToDo }) => {
     const data = todo.map((item, i) => {
       const data = {
         ...item,
-        title: editTitle
+        title: editTitle,
       };
 
       return index == i ? data : item;
     });
 
     setToDo(data);
-    setEdit(null)
+    setEdit(null);
   }
 
   return (
-    <ol className="todo-list">
-      {todo.sort(function(a, b){return a.done - b.done}).map((item, index) => (
-        <li key={index}>
-          {edit == index ? (
-            <div>
-              {index+1}
-              <input
-                defaultValue={item.title}
-                onChange={(e) => setEditTitle(e.target.value)}
-              />
-              <button onClick={()=>onEdit(index)}>
-                save
-              </button>
-            </div>
-          ) : (
-            <p>{index+1}: {item.done ? <del>{item.title}</del> : item.title}</p>
-          )}
-          <div className="list-options">
-            <input
-              onClick={() => onDoneTask(index, item.done)}
-              checked={item.done}
-              type="checkbox"
-            />
-            <button onClick={() => setEdit(index)}>
-              <i className="bi bi-pencil-square"></i>
-            </button>
-            <button onClick={() => ondDelete(index)}>
-              <i className="bi bi-trash3"></i>
-            </button>
-          </div>
-        </li>
-      ))}
-    </ol>
+    <>
+      <input
+        type="search"
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <ol className="todo-list">
+        {searchedData
+          .sort(function (a, b) {
+            return a.done - b.done;
+          })
+          .map((item, index) => (
+            <li key={index}>
+              {edit == index ? (
+                <div>
+                  {index + 1}
+                  <input
+                    defaultValue={item.title}
+                    onChange={(e) => setEditTitle(e.target.value)}
+                  />
+                  <button onClick={() => onEdit(index)}>save</button>
+                </div>
+              ) : (
+                <p>
+                  {index + 1}:{" "}
+                  {item.done ? <del>{item.title}</del> : item.title}
+                </p>
+              )}
+              <div className="list-options">
+                <input
+                  onClick={() => onDoneTask(index, item.done)}
+                  checked={item.done}
+                  type="checkbox"
+                />
+                <button onClick={() => setEdit(index)}>
+                  <i className="bi bi-pencil-square"></i>
+                </button>
+                <button onClick={() => ondDelete(item, index)}>
+                  <i className="bi bi-trash3"></i>
+                </button>
+              </div>
+            </li>
+          ))}
+      </ol>
+    </>
   );
 };
 
